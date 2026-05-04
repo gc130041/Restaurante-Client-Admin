8eb01c9ab8eb213bd25ea474c6d92eff0a3d2322
OlivMer765 <omerida-2021641@kinal.edu.gt>
2026-05-03 23:14:30 -0600

    feat: actualizar rutas de autenticación en el archivo auth.js y .env

--- src/shared/api/auth.js
+++ src/shared/api/auth.js
@@ -1,32 +1,33 @@
 import { axiosAuth } from "./api";
  
 export const login = async (data) => {
-  return await axiosAuth.post("/api/v1/auth/login", data);
+  return await axiosAuth.post("/auth/login", data);
 };
  
 export const register = async (data) => {
-  return await axiosAuth.post("/api/v1/auth/register", data, {
+  return await axiosAuth.post("/auth/register", data, {
     headers: { "Content-Type": "multipart/form-data" }
   });
 };
  
 export const forgotPassword = async (email) => {
-  return await axiosAuth.post("/api/v1/auth/forgot-password", { email });
+  return await axiosAuth.post("/auth/forgot-password", { email });
 };
  
 export const resetPassword = async (token, newPassword) => {
-  return await axiosAuth.post("/api/v1/auth/reset-password", { token, newPassword });
+  return await axiosAuth.post("/auth/reset-password", { token, newPassword });
 };
  
 export const verifyEmail = async (token) => {
-  return await axiosAuth.post("/api/v1/auth/verify-email", { token });
+  return await axiosAuth.post("/auth/verify-email", { token });
 };
  
 export const updateUserRole = async (userId, roleName) => {
-  return await axiosAuth.post(`/api/v1/users/${userId}/role`, { roleName });
+  return await axiosAuth.post(`/users/${userId}/role`, { roleName });
 };
  
 export const getAllUsers = async () => {
-  const { data  } = await axiosAuth.get("/api/v1/auth/users");
+  const { data  } = await axiosAuth.get("/auth/users");
   return { users: data };
 };
+
